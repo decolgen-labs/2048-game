a61e9b471caa43577e28235726d542cb737a65eb
kafierence <vannam34760@gmail.com>
2024-05-22 14:05:52 +0700

    test

--- providers/ProviderStarknet.tsx
+++ providers/ProviderStarknet.tsx
@@ -15,9 +15,6 @@ const ProviderStarknet = ({ children }: PropsWithChildren) => {
   ];
   function rpc() {
     return {
-      // nodeUrl: `https://starknet-mainnet.g.alchemy.com/v2/d9queTbdIieL2dGfWalpnShIHLRtHczH`,
-      // nodeUrl:
-      //   'https://starknet-mainnet.infura.io/v3/7d290a76648a4bac93e5f98aa0d463ce',
       nodeUrl: "https://starknet-sepolia.public.blastapi.io/rpc/v0_7",
       // nodeUrl: "https://free-rpc.nethermind.io/sepolia-juno",
       // nodeUrl: "https://starknet-mainnet.public.blastapi.io/rpc/v0_7",
